9315eeec8b9a5450ae0612b959c697cdab8009c3
sebastiansobieraj <sebastian.sobieraj91@gmail.com>
2018-03-13 22:59:37 +0100

    update5

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://fast-thicket-80115.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://fast-thicket-80115.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
